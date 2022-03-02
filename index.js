b652407c9250926a1916a4f287d8689fe49a8836
orbitlens <orbit.lens@gmail.com>
2022-03-02 16:23:19 +0200

    Revert to CommonJS module type for compatibility

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-export function webpackExcludeNodeModulesExcept(dependentModules) {
-    if (!(dependentModules instanceof Array)) {
-        dependentModules = [dependentModules]
+module.exports = {
+    webpackExcludeNodeModulesExcept(dependentModules) {
+        if (!(dependentModules instanceof Array)) {
+            dependentModules = [dependentModules]
+        }
+        const excludedModules = dependentModules.map(name => name.replace(/\\/, '\\')).join('|')
+        return [
+            new RegExp(`node_modules\\/(?!(${excludedModules})\\/).*`),
+            new RegExp(`node_modules\\/(${excludedModules})\\/node_modules\\/.*`)
+        ]
     }
-    const excludedModules = dependentModules.map(name => name.replace(/\\/, '\\')).join('|')
-    return [
-        new RegExp(`node_modules\\/(?!(${excludedModules})\\/).*`),
-        new RegExp(`node_modules\\/(${excludedModules})\\/node_modules\\/.*`)
-    ]
 }
