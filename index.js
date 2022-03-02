de62230cf023ac66de8689b06a9bfd0da4b47444
orbitlens <orbit.lens@gmail.com>
2022-03-02 15:34:45 +0200

    ES6 module export

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-module.exports = {
-    webpackExcludeNodeModulesExcept(dependentModules) {
-        if (!(dependentModules instanceof Array)) {
-            dependentModules = [dependentModules]
-        }
-        const excludedModules = dependentModules.map(name => name.replace(/\\/, '\\')).join('|')
-        return [
-            new RegExp(`node_modules\\/(?!(${excludedModules})\\/).*`),
-            new RegExp(`node_modules\\/(${excludedModules})\\/node_modules\\/.*`)
-        ]
+export function webpackExcludeNodeModulesExcept(dependentModules) {
+    if (!(dependentModules instanceof Array)) {
+        dependentModules = [dependentModules]
     }
+    const excludedModules = dependentModules.map(name => name.replace(/\\/, '\\')).join('|')
+    return [
+        new RegExp(`node_modules\\/(?!(${excludedModules})\\/).*`),
+        new RegExp(`node_modules\\/(${excludedModules})\\/node_modules\\/.*`)
+    ]
 }
